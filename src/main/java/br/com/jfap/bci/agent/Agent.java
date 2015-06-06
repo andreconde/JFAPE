4d71465f97f53d64e070cde57d8a4032b5e608f1
Andre <conde@corp.insite.com.br>
2015-06-06 15:45:44 -0300

    Log4j incorporado e ajuste no tratamento de excecoes

--- src/main/java/br/com/jfap/bci/agent/Agent.java
+++ src/main/java/br/com/jfap/bci/agent/Agent.java
@@ -3,17 +3,22 @@ package br.com.jfap.bci.agent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 import org.reflections.Reflections;
+
 import br.com.jfap.bci.classreloading.Injector;
 
 public class Agent {
 
+	private static Logger log = Logger.getLogger(Agent.class);
+	
 	public static void premain(String agentArgs, Instrumentation inst) throws InstantiationException, IllegalAccessException {
 		Reflections reflections = new Reflections("");
 		Set<Class<? extends Injector>> classes = reflections.getSubTypesOf(Injector.class);
 		
 		for ( Class<? extends Injector> clazz : classes ) {
-			System.out.println("Registering transformer: " + clazz.getCanonicalName());
+			log.info("Registering transformer: " + clazz.getCanonicalName());
 			inst.addTransformer((ClassFileTransformer) clazz.newInstance());
 		}	
 		
--- src/main/java/br/com/jfap/bci/classreloading/Injector.java
+++ src/main/java/br/com/jfap/bci/classreloading/Injector.java
@@ -4,7 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
-
+import org.apache.log4j.Logger;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.LoaderClassPath;
@@ -20,147 +20,137 @@ import br.com.jfap.bci.annotations.TargetClass;
  *
  */
 public class Injector implements java.lang.instrument.ClassFileTransformer {
-		private ClassPool pool = ClassPool.getDefault();
-		private String workClassCanonicalName;
-		protected CtClass workClass;
-		protected br.com.jfap.bci.pojo.Method method;
-		protected br.com.jfap.bci.pojo.Constructor constructor;
-		
-		public Injector() {
-			try {
-				setWorkClassCanonicalName();
-			} catch ( Exception e ) {
-				e.printStackTrace();
+	private static Logger log = Logger.getLogger(Injector.class);
+	private ClassPool pool = ClassPool.getDefault();
+	private String workClassCanonicalName;
+	protected CtClass workClass;
+	private br.com.jfap.bci.pojo.Method method;
+	protected br.com.jfap.bci.pojo.Constructor constructor;
+
+	public Injector() {
+		try {
+			setWorkClassCanonicalName();
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return the Canonical Name of the Class.
+	 */
+	public String getCanonicalName() {
+		return this.workClassCanonicalName;
+	}
+
+	/**
+	 * 
+	 * @param methodName The name of the method that must be modified via byte code injection 
+	 * @return The method Object specified by the methodName param
+	 * @throws Exception
+	 */
+	private br.com.jfap.bci.pojo.Method getMethod(String methodName) throws Exception {
+		return new br.com.jfap.bci.pojo.Method(workClass.getDeclaredMethod(methodName));
+	}
+
+	private br.com.jfap.bci.pojo.Constructor getConstructor(CtClass[] params) throws Exception {
+		return new br.com.jfap.bci.pojo.Constructor(workClass.getDeclaredConstructor(params));
+	}
+
+	public br.com.jfap.bci.pojo.Method getMethod() {
+		return this.method;
+	}
+
+	/**
+	 * Sets the class that must be modified via byte code injection
+	 * @throws Exception
+	 */
+	private void setWorkClassCanonicalName() throws Exception {
+		log.debug("Setting target Class");
+
+		if ( this.getClass().isAnnotationPresent(TargetClass.class) ) {
+			TargetClass tc = this.getClass().getAnnotation(TargetClass.class);
+			this.workClassCanonicalName = tc.canonicalName();
+		}
+	}
+
+
+	/**
+	 * Gets the class from the pool.
+	 * @throws NotFoundException
+	 */
+	private void setWorkClass() throws NotFoundException {
+		log.debug("Debugging Class Pool...");
+		log.debug("ClassLoaderPool: " + pool.getClassLoader().getClass().getCanonicalName());
+		log.debug("Getting this class from the pool: " + workClassCanonicalName);
+		pool.insertClassPath(new LoaderClassPath(Thread.currentThread().getContextClassLoader()));
+		this.workClass = pool.get(workClassCanonicalName);
+	}
+
+	private void injectMethods() throws Exception {
+		Method[] methods = this.getClass().getMethods();
+
+		for ( Method annotatedMethod : methods ) {
+			Annotation[] methodAnnotations = annotatedMethod.getAnnotations();
+			for ( Annotation a : methodAnnotations ) {
+				if ( a instanceof RewriteMethod ) {
+					this.method = getMethod(((RewriteMethod) a).methodName());
+					log.debug("invoking Method: " + annotatedMethod.getName() + " TargetMethodName: " + this.method.getName());
+					annotatedMethod.invoke(this);
+					log.debug("Class: " + getCanonicalName() + " Method: " + method.getName() + " has been reloaded");
+					break;
+				}
 			}
 		}
-		
-		/**
-		 * @return the Canonical Name of the Class.
-		 */
-		public String getCanonicalName() {
-			return this.workClassCanonicalName;
+	}
+
+	private CtClass[] convertClassToCtClassArray(Class<? extends Object>[] classArray) throws Exception {
+		CtClass[] ctClassArray = new CtClass[classArray.length];
+
+		for ( int i = 0; i < classArray.length; i++) {
+			ctClassArray[i] = pool.get(classArray[i].getCanonicalName());
 		}
 
-		/**
-		 * 
-		 * @param methodName The name of the method that must be modified via byte code injection 
-		 * @return The method Object specified by the methodName param
-		 * @throws Exception
-		 */
-		private br.com.jfap.bci.pojo.Method getMethod(String methodName) throws Exception {
-			return new br.com.jfap.bci.pojo.Method(workClass.getDeclaredMethod(methodName));
-		}
-		
-		private br.com.jfap.bci.pojo.Constructor getConstructor(CtClass[] params) throws Exception {
-			return new br.com.jfap.bci.pojo.Constructor(workClass.getDeclaredConstructor(params));
-		}
-		
-		
-		/**
-		 * Sets the class that must be modified via byte code injection
-		 * @throws Exception
-		 */
-		private void setWorkClassCanonicalName() throws Exception {
-			
-			System.out.println("Setting target Class");
-			
-			if ( this.getClass().isAnnotationPresent(TargetClass.class) ) {
-				TargetClass tc = this.getClass().getAnnotation(TargetClass.class);
-				this.workClassCanonicalName = tc.canonicalName();
-			}
-		}
-		
-		
-		/**
-		 * Gets the class from the pool.
-		 * @throws NotFoundException
-		 */
-		private void setWorkClass() throws NotFoundException {
-			//loadClazz();
-			System.out.println("Debugging Class Pool...");
-			System.out.println("ClassLoaderPool: " + pool.getClassLoader().getClass().getCanonicalName());
-			
-			System.out.println("Getting this class from the pool: " + workClassCanonicalName);
-			pool.insertClassPath(new LoaderClassPath(Thread.currentThread().getContextClassLoader()));
-			this.workClass = pool.get(workClassCanonicalName);
-		}
-		
-		private void injectMethods() {
-			Method[] methods = this.getClass().getMethods();
-			
-			for ( Method annotatedMethod : methods ) {
-				Annotation[] methodAnnotations = annotatedMethod.getAnnotations();
-				for ( Annotation a : methodAnnotations ) {
-					if ( a instanceof RewriteMethod ) {
-						try {
-							this.method = getMethod(((RewriteMethod) a).methodName());
-							System.out.println("invoking Method: " + annotatedMethod.getName() + " TargetMethodName: " + this.method.getName());
-							annotatedMethod.invoke(this);
-							System.out.println("Class: " + getCanonicalName() + " Method: " + method.getName() + " has been reloaded");
-						} catch (Exception e) {
-							e.printStackTrace();
-						} 
-						break;
-					}
-				}
-			}
-		}
-		
-		private CtClass[] convertClassToCtClassArray(Class<? extends Object>[] classArray) throws Exception {
-			CtClass[] ctClassArray = new CtClass[classArray.length];
-			
-			for ( int i = 0; i < classArray.length; i++) {
-				
-				ctClassArray[i] = pool.get(classArray[i].getCanonicalName());
-			}
-			
-			return ctClassArray;
-		}
-		
-		private void injectConstructors() {
-			Method[] methods = this.getClass().getMethods();
-			
-			for ( Method annotatedMethod : methods ) {
-				Annotation[] methodAnnotations = annotatedMethod.getAnnotations();
-				for ( Annotation a : methodAnnotations ) {
-					if ( a instanceof RewriteConstructor ) {
-						try {
-							this.constructor =  getConstructor(convertClassToCtClassArray(((RewriteConstructor) a).params()));
-							System.out.println("invoking Constructor: " + annotatedMethod.getName() + " TargetConstructorName: " + this.constructor.getName());
-							annotatedMethod.invoke(this);
-							System.out.println("Class: " + getCanonicalName() + " Constructor: " + constructor.getName() + " has been reloaded");
-						} catch (Exception e) {
-							e.printStackTrace();
-						} 
-						break;
-					}
-				}
-			}
-			
-		}
-		
+		return ctClassArray;
+	}
 
-		@Override
-		public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-			byte[] byteCode = classfileBuffer;
-			
-			try {
-				
-				if ( className.equalsIgnoreCase(this.workClassCanonicalName) ) {
-					System.out.println("className: " + className);
-					System.out.println("this.clazz: " + this.workClassCanonicalName);
-					setWorkClass();
-					injectMethods();
-					injectConstructors();
-					byteCode = this.workClass.toBytecode();
-					this.workClass.detach();
+	private void injectConstructors() throws Exception {
+		Method[] methods = this.getClass().getMethods();
+
+		for ( Method annotatedMethod : methods ) {
+			Annotation[] methodAnnotations = annotatedMethod.getAnnotations();
+			for ( Annotation a : methodAnnotations ) {
+				if ( a instanceof RewriteConstructor ) {
+					this.constructor =  getConstructor(convertClassToCtClassArray(((RewriteConstructor) a).params()));
+					log.debug("invoking Constructor: " + annotatedMethod.getName() + " TargetConstructorName: " + this.constructor.getName());
+					annotatedMethod.invoke(this);
+					log.debug("Class: " + getCanonicalName() + " Constructor: " + constructor.getName() + " has been reloaded");
+					break;
 				}
-			} catch ( Throwable t ) {
-				t.printStackTrace();
-				throw new RuntimeException("Erro ao iniciar jfap agent");
 			}
-			
-			return byteCode;
 		}
-	
+
+	}
+
+
+	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+		byte[] byteCode = classfileBuffer;
+
+		try {
+			if ( className.equalsIgnoreCase(this.workClassCanonicalName) ) {
+				log.debug("className: " + className);
+				log.debug("this.clazz: " + this.workClassCanonicalName);
+				setWorkClass();
+				injectMethods();
+				injectConstructors();
+				byteCode = this.workClass.toBytecode();
+				this.workClass.detach();
+			}
+		} catch ( Throwable t ) {
+			t.printStackTrace();
+			throw new RuntimeException("Erro ao iniciar jfap agent");
+		}
+
+		return byteCode;
+	}
+
 }
